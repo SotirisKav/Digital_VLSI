5847b08732719662e1e7482e98fa7b5ad429e77d
SotirisKav <sotiriskavadakis@gmail.com>
2026-03-22 17:12:50 +0200

    mac module complete

--- lab3/lab3.srcs/sources_1/new/mac.vhd
+++ lab3/lab3.srcs/sources_1/new/mac.vhd
@@ -1,43 +1,38 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/20/2026 02:42:10 PM
--- Design Name: 
--- Module Name: mac - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.math_real.all;
 
 entity mac is
---  Port ( );
+    generic (
+        data_width : integer := 8; -- this is N (num bits of x[n])
+        coeff_width : integer := 8; -- this is N (num bits of h[n])
+        num_taps : integer := 8 -- this is M 
+    );
+
+    port (
+        clk : in std_logic;
+        rom_out : in std_logic_vector(coeff_width-1 downto 0);
+        ram_out : in std_logic_vector(data_width-1 downto 0);
+        mac_init : in std_logic;
+        L : out std_logic_vector(data_width + coeff_width + integer(ceil(log2(real(num_taps)))) - 1 downto 0)
+    );
 end mac;
 
 architecture Behavioral of mac is
-
+    signal acc : std_logic_vector(data_width + coeff_width + integer(ceil(log2(real(num_taps)))) - 1 downto 0) := (others => '0');
 begin
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            if mac_init = '1' then -- begin a new calculation
+                acc <= (others => '0'); -- keep in mind that acc is 2N + 3 bits but ram and rom are each N bits!
+                acc(data_width + coeff_width - 1 downto 0) <= ram_out * rom_out; -- initialize accumulator with the first product
+            else
+                acc <= acc + (ram_out * rom_out); -- accumulate the product
+            end if;
+        end if;
+    end process;
 
-
+    L <= acc;
 end Behavioral;
